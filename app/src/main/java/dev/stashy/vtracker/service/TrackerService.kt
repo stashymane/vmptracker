0857b1e71e4d34a89030f49badfc14db14c63045
stashymane <aestheticrice@gmail.com>
2024-10-13 09:19:44 +0300

    add frames/results to tracker interface

--- app/src/main/java/dev/stashy/vtracker/service/TrackerService.kt
+++ app/src/main/java/dev/stashy/vtracker/service/TrackerService.kt
@@ -1,16 +1,20 @@
 package dev.stashy.vtracker.service
 
+import androidx.camera.core.ImageProxy
+import dev.stashy.vtracker.service.tracking.FaceTracker
+import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.flow.StateFlow
 
 interface TrackerService {
     sealed class Status {
         object NotRunning : Status()
-        object Starting : Status()
         object Running : Status()
         class Error(val exception: Exception) : Status()
     }
 
     val status: StateFlow<Status>
+    val frames: ReceiveChannel<ImageProxy>
+    val results: ReceiveChannel<Result<FaceTracker.Frame>>
 
     fun start()
     fun stop()
--- app/src/main/java/dev/stashy/vtracker/service/TrackerServiceImpl.kt
+++ app/src/main/java/dev/stashy/vtracker/service/TrackerServiceImpl.kt
@@ -6,6 +6,7 @@ import android.content.pm.ServiceInfo
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
+import androidx.camera.core.ImageProxy
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.NotificationCompat
 import androidx.core.app.ServiceCompat
@@ -14,6 +15,7 @@ import dev.stashy.vtracker.service.TrackerService.Status
 import dev.stashy.vtracker.service.tracking.FaceTracker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -23,7 +25,11 @@ class TrackerServiceImpl : Service(), TrackerService, CoroutineScope by Coroutin
 
     private val _status: MutableStateFlow<Status> = MutableStateFlow(Status.NotRunning)
     override val status: StateFlow<Status> = _status
-    val faceTrackResults get() = faceTracker.results
+
+    override val frames: ReceiveChannel<ImageProxy>
+        get() = TODO("Not yet implemented")
+
+    override val results get() = faceTracker.results
 
     /**
      * Starts the service from a received intent, and sets this up as a foreground service.
