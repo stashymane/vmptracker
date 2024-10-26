14b23bd5fe66aa2b118e33bca9077c62b77ccdf2
stashymane <aestheticrice@gmail.com>
2024-10-26 08:59:21 +0300

    split off camera control into own component

---
+++ app/src/main/java/dev/stashy/vtracker/ui/component/CameraControlRow.kt
@@ -0,0 +1,98 @@
+package dev.stashy.vtracker.ui.component
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Stop
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.stashy.vtracker.R
+import dev.stashy.vtracker.ui.theme.VTrackerTheme
+
+@Composable
+fun CameraControlRow(
+    isActive: Boolean = false,
+    onPickLens: () -> Unit,
+    onStart: () -> Unit,
+    onSettings: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val startButtonColor by animateColorAsState(
+        if (isActive)
+            MaterialTheme.colorScheme.primary
+        else MaterialTheme.colorScheme.surfaceContainer, label = "run button color"
+    )
+    val startButtonContentColor by animateColorAsState(
+        if (isActive)
+            MaterialTheme.colorScheme.onPrimary
+        else MaterialTheme.colorScheme.onSurface, label = "run button content color"
+    )
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .padding(horizontal = 32.dp, vertical = 16.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(16.dp)
+    ) {
+        IconButton(onPickLens) {
+            Icon(Icons.Default.Cameraswitch, "Lens change")
+        }
+
+        Button(
+            onStart,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = startButtonColor,
+                contentColor = startButtonContentColor
+            )
+        ) {
+            AnimatedContent(isActive, label = "start button text") {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (it) {
+                        Icon(imageVector = Icons.Default.Stop, null)
+                        Text(stringResource(R.string.stop_button))
+                    } else {
+                        Icon(imageVector = Icons.Default.PlayArrow, null)
+                        Text(stringResource(R.string.start_button))
+                    }
+                }
+            }
+        }
+
+        IconButton(onSettings) {
+            Icon(Icons.Default.Settings, contentDescription = "Settings")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CameraControlRowPreview() {
+    VTrackerTheme {
+        CameraControlRow(false, {}, {}, {})
+    }
+}
--- app/src/main/java/dev/stashy/vtracker/ui/screen/PreviewScreen.kt
+++ app/src/main/java/dev/stashy/vtracker/ui/screen/PreviewScreen.kt
@@ -2,27 +2,13 @@ package dev.stashy.vtracker.ui.screen
 
 import androidx.camera.compose.CameraXViewfinder
 import androidx.camera.viewfinder.surface.ImplementationMode
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Cameraswitch
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Stop
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -32,14 +18,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LifecycleStartEffect
 import androidx.lifecycle.compose.LocalLifecycleOwner
-import dev.stashy.vtracker.R
 import dev.stashy.vtracker.model.settings.GeneralSettings
 import dev.stashy.vtracker.service.TrackerService
+import dev.stashy.vtracker.ui.component.CameraControlRow
 import dev.stashy.vtracker.ui.component.LocalNavController
 import dev.stashy.vtracker.ui.component.dialog.CameraChoiceContent
 import dev.stashy.vtracker.ui.theme.VTrackerTheme
@@ -62,19 +46,6 @@ fun PreviewScreen(
     var showCameraPicker by remember { mutableStateOf(false) }
     val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
-    val startButtonColor by animateColorAsState(
-        when (status) {
-            is TrackerService.Status.Running -> MaterialTheme.colorScheme.primary
-            else -> MaterialTheme.colorScheme.surfaceContainer
-        }, label = "run button color"
-    )
-    val startButtonContentColor by animateColorAsState(
-        when (status) {
-            is TrackerService.Status.Running -> MaterialTheme.colorScheme.onPrimary
-            else -> MaterialTheme.colorScheme.onSurface
-        }, label = "run button content color"
-    )
-
     LifecycleStartEffect(generalSettings.cameraId) {
         vm.startPreview(lifecycleOwner, generalSettings.cameraId ?: "1", 0)
         onStopOrDispose {
@@ -96,57 +67,16 @@ fun PreviewScreen(
                 .padding(contentPadding)
                 .fillMaxSize()
         ) {
-            IconButton(
-                { showCameraPicker = true },
-                Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(32.dp)
-            ) {
-                Icon(Icons.Default.Cameraswitch, "Lens change")
-            }
-
-            Button(
-                {
+            CameraControlRow(modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth(),
+                isActive = status is TrackerService.Status.Running,
+                onPickLens = { showCameraPicker = true },
+                onStart = {
                     if (status is TrackerService.Status.Running) vm.stop()
                     else vm.start()
                 },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = startButtonColor,
-                    contentColor = startButtonContentColor
-                ),
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(32.dp)
-            ) {
-                AnimatedContent(status, label = "start button text") {
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(12.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        when (it) {
-                            is TrackerService.Status.Running -> {
-                                Icon(imageVector = Icons.Default.Stop, null)
-                                Text(stringResource(R.string.stop_button))
-                            }
-
-                            is TrackerService.Status.NotRunning,
-                            is TrackerService.Status.Error -> {
-                                Icon(imageVector = Icons.Default.PlayArrow, null)
-                                Text(stringResource(R.string.start_button))
-                            }
-                        }
-                    }
-                }
-            }
-
-            IconButton(
-                { navController.navigate(Screen.Settings) },
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .padding(32.dp)
-            ) {
-                Icon(Icons.Default.Settings, contentDescription = "Settings")
-            }
+                onSettings = { navController.navigate(Screen.Settings) })
         }
     }
 
