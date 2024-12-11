a260f81fd24621aff2d420cde055bb6dba6b367d
stashymane <aestheticrice@gmail.com>
2024-12-11 18:57:35 +0200

    add default args for blank trackerframe

--- app/src/main/java/dev/stashy/vtracker/model/tracking/TrackerFrame.kt
+++ app/src/main/java/dev/stashy/vtracker/model/tracking/TrackerFrame.kt
@@ -3,7 +3,7 @@ package dev.stashy.vtracker.model.tracking
 import kotlin.time.Duration
 
 data class TrackerFrame(
-    val result: List<TrackingData>,
-    val processingTime: Duration,
-    val imageSize: Pair<Int, Int>,
+    val result: List<TrackingData> = listOf(),
+    val processingTime: Duration = Duration.ZERO,
+    val imageSize: Pair<Int, Int> = 0 to 0,
 )
